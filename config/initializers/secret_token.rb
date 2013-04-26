c8e159841b5a9aaec7cc8ff5d0c2d3b57bd9030c
Manuel Estebanez <manu3d@iname.com>
2013-04-26 19:49:23 +0200

    inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Danceattendance::Application.config.secret_token = '4f3ccda500bda0178b17cfd98caa7cf93aa6733755d556842253a3b4ff42ad36cce193c77b752c9539ffa9a71fd7a5f14f208cf6de72e4ee0b9f15be9b032c62'
